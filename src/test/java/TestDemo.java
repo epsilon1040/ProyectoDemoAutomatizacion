038c64d73ab3b2cd76b45370dbaaf4fff168e339
1415506 <1415506@50HW013510.SOAM.TCS.com>
2018-01-25 17:31:31 -0500

    DemoWs - Automatización - modificación url firefox

--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -37,8 +37,8 @@ public class TestDemo {
     
     @Before
     public void setUp() throws IOException {
-      //URL serverurl = new URL("https://boya678:6db6a871-5bfd-456f-b8b1-c0195cb26b2d@ondemand.saucelabs.com:443/wd/hub");
-       URL serverurl = new URL("http://127.0.0.1:4444/wd/hub");  
+      URL serverurl = new URL("https://boya678:6db6a871-5bfd-456f-b8b1-c0195cb26b2d@ondemand.saucelabs.com:443/wd/hub");
+      // URL serverurl = new URL("http://127.0.0.1:4444/wd/hub");  
             DesiredCapabilities capabilities =new DesiredCapabilities();
             capabilities.setCapability("browserName", "chrome");
             driver = new RemoteWebDriver(serverurl,capabilities);
